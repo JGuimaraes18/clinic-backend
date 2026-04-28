842c144c53db8746cedbd659f0fa696dabcc0891
JGuimaraes18 <jpedro.guimaraes@hotmail.com>
2026-04-28 16:37:24 -0300

    fix: include search name

--- backend/apps/appointments/serializers.py
+++ backend/apps/appointments/serializers.py
@@ -3,6 +3,9 @@ from .models import Atendimento
 
 
 class AtendimentoSerializer(serializers.ModelSerializer):
+    paciente_nome = serializers.CharField(source="paciente.full_name", read_only=True)
+    profissional_nome = serializers.CharField(source="profissional.full_name", read_only=True)
+
     class Meta:
         model = Atendimento
         fields = "__all__"
